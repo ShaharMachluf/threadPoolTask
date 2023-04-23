66f37322491c79a795167cd73a3fed35faaaaf35
ShaharMachluf <shahar6261@gmail.com>
2023-04-23 15:06:03 +0300

    data stractured ready

--- main.cpp
+++ main.cpp
@@ -2,15 +2,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <iostream>
+#include <queue>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <pthread.h>
+
+struct task{
+    char* txt;
+    int key;
+    int index;
+}Task;
+
+int writeIndex;
+std::queue<struct task> taskQ;
+std::queue<pthread_t*> threadQ;
 
 int main(int argc, char *argv[])
 {
+    int readIndex = 0;
+    int cores = get_nprocs_conf();
+    writeIndex = 0;
+
 	if (argc != 2)
 	{
 	    printf("usage: key < file \n");
 	    printf("!! data more than 1024 char will be ignored !!\n");
 	    return 0;
 	}
+
+    for(int i=0; i<cores; i++){
+        threadQ.push(new pthread_t);
+    }
 
 	int key = atoi(argv[1]);
 	printf("key is %i \n",key);
@@ -27,10 +50,16 @@ int main(int argc, char *argv[])
 	  counter++;
 
 	  if (counter == 1024){
-		encrypt(data,key);
-		printf("encripted data: %s\n",data);
+        struct task *t = (struct task*)malloc(sizeof(struct task));
+        t->txt = data;
+        t->key = key;
+        t->txt = data;
+        taskQ.push(*t);
+		// encrypt(data,key);
+		// printf("encripted data: %s\n",data);
 		counter = 0;
 	  }
+      readIndex++;
 	}
 	
 	if (counter > 0)
@@ -38,9 +67,21 @@ int main(int argc, char *argv[])
 		char lastData[counter];
 		lastData[0] = '\0';
 		strncat(lastData, data, counter);
-		encrypt(lastData,key);
-		printf("encripted data:\n %s\n",lastData);
+        struct task *t = (struct task*)malloc(sizeof(struct task));
+        t->txt = lastData;
+        t->key = key;
+        t->txt = data;
+        taskQ.push(*t);
+		// encrypt(lastData,key);
+		// printf("encripted data:\n %s\n",lastData);
+        readIndex++;
 	}
+
+    for(int i=0; i<cores; i++){
+        pthread_t* curr = threadQ.front();
+        threadQ.pop();
+        delete(curr);
+    }
 
 	return 0;
 }
